76e74b0e929baba416f79dc7b135c09d54cf86d6
Samekuma <samuelekumamba@gmail.com>
2024-07-22 15:10:24 +0100

    Added the footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeDetailsComponent } from './home-details/home-details.component';
 import { HomeTrustedBusinessesComponent } from './home-trusted-businesses/home-trusted-businesses.component';
 import { HomeSupportedBrandedComponent } from './home-supported-branded/home-supported-branded.component';
 import { HomeGlobalBrandComponent } from './home-global-brand/home-global-brand.component';
+import { HomeCtaComponent } from './home-cta/home-cta.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeHeroComponent, HomeDetailsComponent, HomeTrustedBusinessesComponent, HomeSupportedBrandedComponent, HomeGlobalBrandComponent],
+  declarations: [HomeComponent, HomeHeroComponent, HomeDetailsComponent, HomeTrustedBusinessesComponent, HomeSupportedBrandedComponent, HomeGlobalBrandComponent, HomeCtaComponent],
   imports: [CommonModule],
   exports: [HomeComponent, HomeHeroComponent],
 })
